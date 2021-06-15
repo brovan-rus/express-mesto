c3462e5efcedc979ea1318c12c9e8acd182f1212
Konstantin Brovtsev <brovan@yandex.ru>
2021-06-15 04:41:11 +0300

    fix: db connection error message

--- app.js
+++ app.js
@@ -8,21 +8,17 @@ const cardRoutes = require('./routes/card');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use((req, res, next) => {
   req.user = {
     _id: '60c7a640dcd9901f383c3453',
   };
   next();
 });
-
 app.use('/users', userRoutes);
 app.use('/cards', cardRoutes);
-
 app.listen(port, () => {
   console.log(`We are live on ${port}`);
 });
-
 mongoose
   .connect('mongodb://localhost:27017/mestodb', {
     useNewUrlParser: true,
@@ -30,4 +26,4 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error(err));
+  .catch((err) => console.error(`Произошла ошибка подключения к базе данных ${err}`));
