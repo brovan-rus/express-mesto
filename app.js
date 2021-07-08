a3a955e9bd9f74555de79855fd8de7579fa05d22
Konstantin Brovtsev <brovan@yandex.ru>
2021-07-08 22:02:23 +0300

    feat: add crush test

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signin', validateLoginRequest, login);
 app.post('/signup', validateRegisterRequest, createUser);
 app.use(auth);
